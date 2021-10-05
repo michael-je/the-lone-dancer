eacf7a0e561a921c2de4ed741c5edb700663643e
morgaesis <morgaesis@noreply.users.github.com>
2021-10-05 16:07:10 +0000

    bot.py: Logging and add __main__

--- bot.py
+++ bot.py
@@ -1,20 +1,49 @@
-import discord
+"""
+Author MikkMakk88, morgaesis et al.
+
+(c)2021
+"""
+
+import os
+import configparser
+import logging
+
+import discord  # pylint: disable=import-error
 
 
 class MusicBot(discord.Client):
-    def __init__(self):
-        super().__init__()
+    """
+    The main bot functionality
+    """
 
-    decorator_helper = discord.Client()
+    client = discord.Client()
 
-    @decorator_helper.event
+    @client.event
     async def on_ready(self):
-        print(f"we have logged in as {self.user}")
+        """ Login and loading handling """
+        logging.info("we have logged in as %s", self.user)
 
-    @decorator_helper.event
+    @client.event
     async def on_message(self, message):
+        """ Handler for receiving messages """
         if message.author == self.user:
             return
         if message.content.startswith('!hello'):
             await message.channel.send('Hello!')
 
+
+if __name__ == '__main__':
+    print("Starting Discord bot")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    token = os.getenv('TOKEN')
+    if token is None:
+        config = configparser.ConfigParser()
+        config.read(".env")
+        token = config['SECRETS']['TOKEN']
+
+    logging.info("Starting bot")
+    MusicBot().run(token)

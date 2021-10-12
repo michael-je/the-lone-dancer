666e729361e2962115cef4683b8921dafa9b806b
Michael Joseph <michaeljoseph195@gmail.com>
2021-10-12 02:23:50 +0000

    Rearrange bot methods

--- bot.py
+++ bot.py
@@ -27,11 +27,11 @@ class MusicBot(discord.Client):
 
     def __init__(self):
         self.handlers = {}
-
-        self.register_command("hello", handler=self.hello)
-        self.register_command("countdown", handler=self.countdown)
-        self.register_command("dinkster", handler=self.dinkster)
-        self.register_command("joke", handler=self.joke)
+        self.voice_client = None
+        self.song_queue = queue.Queue()
+        self.url_regex = re.compile(
+            "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+        )
 
         self.register_command("play", handler=self.play)
         self.register_command("stop", handler=self.stop)
@@ -39,18 +39,12 @@ class MusicBot(discord.Client):
         self.register_command("resume", handler=self.resume)
         self.register_command("skip", handler=self.skip)
 
-        self.voice_client = None
-        self.song_queue = queue.Queue()
-        self.url_regex = re.compile(
-            "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-        )
+        self.register_command("hello", handler=self.hello)
+        self.register_command("countdown", handler=self.countdown)
+        self.register_command("dinkster", handler=self.dinkster)
+        self.register_command("joke", handler=self.joke)
 
         super().__init__()
-
-    def next_in_queue(self, error):
-        if self.song_queue.empty() == False:
-            self.voice_client.stop()
-            self.voice_client.play(self.song_queue.get(), after=self.next_in_queue)
 
     def register_command(self, command_name, handler=None):
         """Register a command with the name 'command_name'.
@@ -95,6 +89,80 @@ class MusicBot(discord.Client):
             return None, None, f"Command {command_name} not recognized."
 
         return self.handlers[command_name], command_content, None
+
+    _discord_helper = discord.Client()
+
+    @_discord_helper.event
+    async def on_ready(self):
+        """Login and loading handling"""
+        logging.info("we have logged in as %s", self.user)
+
+    @_discord_helper.event
+    async def on_message(self, message):
+        """Handler for receiving messages"""
+        if message.author == self.user:
+            return
+
+        if not message.content:
+            return
+
+        if message.content[0] != MusicBot.COMMAND_PREFIX:
+            # Message not attempting to be a command.
+            return
+
+        handler, command_content, error_msg = self.get_command_handler(message.content)
+
+        # The command was not recognized
+        if error_msg:
+            return await message.channel.send(error_msg)
+
+        # Execute the command.
+        await handler(message, command_content)
+
+    def next_in_queue(self, error):
+        if self.song_queue.empty() == False:
+            self.voice_client.stop()
+            self.voice_client.play(self.song_queue.get(), after=self.next_in_queue)
+
+    async def play(self, message, command_content):
+        video_metadata = None
+
+        if self.url_regex.match(command_content):
+            video_metadata = pafy.new(command_content)
+        else:
+            search_result = VideosSearch(command_content).result()
+            video_metadata = pafy.new(search_result["result"][0]["id"])
+
+        logging.info("Pafy found", self.user)
+        logging.info(str(video_metadata))
+        audio_url = video_metadata.getbestaudio().url
+
+        if self.voice_client is None:
+            self.voice_client = await message.author.voice.channel.connect()
+
+        audio_source = discord.FFmpegPCMAudio(audio_url)
+
+        if self.voice_client.is_playing():
+            self.song_queue.put(audio_source)
+            await message.channel.send("Added to Queue: " + video_metadata.title)
+        else:
+            self.voice_client.play(audio_source, after=self.next_in_queue)
+            await message.channel.send("Now Playing: " + video_metadata.title)
+
+    async def stop(self, message, command_content):
+        if self.voice_client:
+            self.voice_client.stop()
+
+    async def pause(self, message, command_content):
+        if self.voice_client:
+            self.voice_client.pause()
+
+    async def resume(self, message, command_content):
+        if self.voice_client:
+            self.voice_client.resume()
+
+    async def skip(self, message, command_content):
+        self.next_in_queue(None)
 
     async def hello(self, message, command_content):
         await message.channel.send("Hello!")
@@ -183,75 +251,6 @@ class MusicBot(discord.Client):
             await message.channel.send(joke["setup"])
             await asyncio.sleep(joke_pause)
             await message.channel.send(joke["delivery"])
-
-    async def play(self, message, command_content):
-        video_metadata = None
-
-        if self.url_regex.match(command_content):
-            video_metadata = pafy.new(command_content)
-        else:
-            search_result = VideosSearch(command_content).result()
-            video_metadata = pafy.new(search_result["result"][0]["id"])
-
-        logging.info("Pafy found", self.user)
-        logging.info(str(video_metadata))
-        audio_url = video_metadata.getbestaudio().url
-
-        if self.voice_client is None:
-            self.voice_client = await message.author.voice.channel.connect()
-
-        audio_source = discord.FFmpegPCMAudio(audio_url)
-
-        if self.voice_client.is_playing():
-            self.song_queue.put(audio_source)
-            await message.channel.send("Added to Queue: " + video_metadata.title)
-        else:
-            self.voice_client.play(audio_source, after=self.next_in_queue)
-            await message.channel.send("Now Playing: " + video_metadata.title)
-
-    async def stop(self, message, command_content):
-        if self.voice_client:
-            self.voice_client.stop()
-
-    async def pause(self, message, command_content):
-        if self.voice_client:
-            self.voice_client.pause()
-
-    async def resume(self, message, command_content):
-        if self.voice_client:
-            self.voice_client.resume()
-
-    async def skip(self, message, command_content):
-        self.next_in_queue(None)
-
-    _discord_helper = discord.Client()
-
-    @_discord_helper.event
-    async def on_ready(self):
-        """Login and loading handling"""
-        logging.info("we have logged in as %s", self.user)
-
-    @_discord_helper.event
-    async def on_message(self, message):
-        """Handler for receiving messages"""
-        if message.author == self.user:
-            return
-
-        if not message.content:
-            return
-
-        if message.content[0] != MusicBot.COMMAND_PREFIX:
-            # Message not attempting to be a command.
-            return
-
-        handler, command_content, error_msg = self.get_command_handler(message.content)
-
-        # The command was not recognized
-        if error_msg:
-            return await message.channel.send(error_msg)
-
-        # Execute the command.
-        await handler(message, command_content)
 
 
 if __name__ == "__main__":

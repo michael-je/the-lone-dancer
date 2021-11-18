5b108b1fc27862c6a14eb0c2f6cbbf675cca4668
Michael Joseph <michaeljoseph195@gmail.com>
2021-11-18 20:51:10 +0000

    fix mistake from #47

--- bot.py
+++ bot.py
@@ -451,7 +451,8 @@ class MusicBot:
         if self.voice_client.is_playing():
             return
 
-        if self.last_played_time < self.DISCONNECT_TIMER_SECONDS:
+        time_since_last_played = time.time() - self.last_played_time
+        if time_since_last_played < self.DISCONNECT_TIMER_SECONDS:
             return
 
         self._stop()

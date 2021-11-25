dab7c03ef86183bb9f93f7c953c73bc50585362b
Michael Joseph <michaeljoseph195@gmail.com>
2021-11-25 13:50:30 +0000

    add default value for like_count as well

--- pafy_fixed/backend_youtube_dl_fixed.py
+++ pafy_fixed/backend_youtube_dl_fixed.py
@@ -47,8 +47,8 @@ class YtdlPafyFixed(YtdlPafy):
         self._rating = self._ydl_info['average_rating']
         self._length = self._ydl_info['duration']
         self._viewcount = self._ydl_info['view_count']
-        self._likes = self._ydl_info['like_count']
-        # added a default value for dislike_count
+        # added a default value for like_count and dislike_count
+        self._likes = self._ydl_info.get('like_count', 0)
         self._dislikes = self._ydl_info.get('dislike_count', 0)
         self._username = self._ydl_info['uploader_id']
         self._category = self._ydl_info['categories'][0] if self._ydl_info['categories'] else ''
